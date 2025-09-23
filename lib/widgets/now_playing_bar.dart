48a701d9aa890e7cab6db680cc5fc1038375f7f1
Nioduro <mickkcx@proton.me>
2025-09-23 15:10:14 +0200

    feat: add shuffle and repeat controls to now playing bar

--- lib/widgets/now_playing_bar.dart
+++ lib/widgets/now_playing_bar.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:vibin_app/audio/audio_manager.dart';
 import 'package:vibin_app/main.dart';
 
@@ -8,19 +9,21 @@ class NowPlayingBar extends StatefulWidget {
   const NowPlayingBar({super.key});
 
   @override
-  State<NowPlayingBar> createState() => _NowPlayingBarContext();
+  State<NowPlayingBar> createState() => _NowPlayingBarState();
 }
 
-class _NowPlayingBarContext extends State<NowPlayingBar> {
+class _NowPlayingBarState extends State<NowPlayingBar> {
 
   final AudioManager audioManager = getIt<AudioManager>();
   late var isPlaying = audioManager.audioPlayer.playing;
   late var position = audioManager.audioPlayer.position;
+  late var shuffleEnabled = audioManager.audioPlayer.shuffleModeEnabled;
+  late var repeatMode = audioManager.audioPlayer.loopMode;
   late var currentMediaItem = audioManager.getCurrentMediaItem();
 
   List<StreamSubscription> subscriptions = [];
 
-  _NowPlayingBarContext() {
+  _NowPlayingBarState() {
     subscriptions.add(audioManager.audioPlayer.playingStream.listen((event) {
       setState(() {
         isPlaying = event;
@@ -34,6 +37,16 @@ class _NowPlayingBarContext extends State<NowPlayingBar> {
     subscriptions.add(audioManager.audioPlayer.sequenceStateStream.listen((event) {
       setState(() {
         currentMediaItem = audioManager.getCurrentMediaItem();
+      });
+    }));
+    subscriptions.add(audioManager.audioPlayer.shuffleModeEnabledStream.listen((event) {
+      setState(() {
+        shuffleEnabled = event;
+      });
+    }));
+    subscriptions.add(audioManager.audioPlayer.loopModeStream.listen((event) {
+      setState(() {
+        repeatMode = event;
       });
     }));
   }
@@ -66,9 +79,41 @@ class _NowPlayingBarContext extends State<NowPlayingBar> {
     audioManager.audioPlayer.seek(Duration(milliseconds: milliseconds.toInt()));
   }
 
+  void toggleShuffle() {
+    audioManager.audioPlayer.setShuffleModeEnabled(!shuffleEnabled);
+  }
+
+  void toggleRepeat() {
+    if (audioManager.audioPlayer.loopMode == LoopMode.off) {
+      audioManager.audioPlayer.setLoopMode(LoopMode.all);
+    } else if (audioManager.audioPlayer.loopMode == LoopMode.all) {
+      audioManager.audioPlayer.setLoopMode(LoopMode.one);
+    } else {
+      audioManager.audioPlayer.setLoopMode(LoopMode.off);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
+    final showExtendedControls = MediaQuery.sizeOf(context).width > 600;
+    return currentMediaItem == null ? const SizedBox.shrink() : GestureDetector(
+      onHorizontalDragEnd: (details) {
+        if (details.primaryVelocity == null) return;
+        if (details.primaryVelocity! < 0) {
+          skipNext();
+        } else if (details.primaryVelocity! > 0) {
+          skipPrevious();
+        }
+      },
+      onVerticalDragEnd: (details) {
+        if (details.primaryVelocity == null) return;
+        if (details.primaryVelocity! < 0) {
+          Navigator.of(context).pushNamed("/nowplaying");
+        }
+        else if (details.primaryVelocity! > 0) {
+          audioManager.audioPlayer.stop();
+        }
+      },
       onTap: () {
 
       },
@@ -94,33 +139,53 @@ class _NowPlayingBarContext extends State<NowPlayingBar> {
             height: 60,
             color: Theme.of(context).colorScheme.surface,
             child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  children: [
-                    if (currentMediaItem?.artUri != null) ...[
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
-                        child: Image.network(
-                          currentMediaItem!.artUri.toString(),
-                          headers: currentMediaItem?.artHeaders ?? {},
-                          width: 44,
-                          height: 44
-                        ),
-                      )
-                    ],
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(currentMediaItem?.title ?? "", style: Theme.of(context).textTheme.bodyLarge),
-                        Text(currentMediaItem?.artist ?? "", style: Theme.of(context).textTheme.bodyMedium),
-                    ]),
-                  ],
+                if (currentMediaItem?.artUri != null) ...[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
+                    child: Image.network(
+                      currentMediaItem!.artUri.toString(),
+                      headers: currentMediaItem?.artHeaders ?? {},
+                      width: 44,
+                      height: 44
+                    ),
+                  )
+                ],
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                          currentMediaItem!.title,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.bodyLarge
+                      ),
+                      Text(
+                          currentMediaItem!.artist ?? "",
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                  ]),
                 ),
                 Row(
                   children: [
+                    if (showExtendedControls) ... [
+                      IconButton(
+                          onPressed: toggleRepeat,
+                          icon: switch(repeatMode) {
+                            LoopMode.off => Icon(Icons.repeat, color: Theme.of(context).colorScheme.onSurface),
+                            LoopMode.all => Icon(Icons.repeat, color: Theme.of(context).colorScheme.primary),
+                            LoopMode.one => Icon(Icons.repeat_one, color: Theme.of(context).colorScheme.primary)
+                          }
+                      ),
+                      IconButton(onPressed: toggleShuffle, icon: Icon(Icons.shuffle), color: shuffleEnabled ? Theme.of(context).colorScheme.primary : Theme.of(context).colorScheme.onSurface),
+                      SizedBox(width: 16)
+                    ],
                     IconButton(onPressed: skipPrevious, icon: const Icon(Icons.skip_previous)),
                     IconButton(onPressed: playPause, icon: isPlaying ? Icon(Icons.pause) : Icon(Icons.play_arrow)),
                     IconButton(onPressed: skipNext, icon: const Icon(Icons.skip_next)),
