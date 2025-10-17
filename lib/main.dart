83ebae8798c8ea0da44f2a9e658ed33cd26c5e17
Nioduro <mickkcx@proton.me>
2025-10-18 01:52:36 +0200

    fix: prevent crash caused by checking platform on web

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:dbus/dbus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get_it/get_it.dart';
@@ -30,7 +31,7 @@ void main() async {
 
   await AudioManager.initBackgroundTask();
 
-  if (Platform.isLinux) {
+  if (!kIsWeb && Platform.isLinux) {
     final session = DBusClient.session();
     final mpris = MprisPlayer();
 
