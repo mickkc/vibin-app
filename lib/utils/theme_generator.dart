6d6b2c4677bca22461294d6d8c02970fce7627e0
Nioduro <mickkcx@proton.me>
2025-10-18 15:53:14 +0200

    fix: add options for text color on primary, secondary and error

--- lib/utils/theme_generator.dart
+++ lib/utils/theme_generator.dart
@@ -8,20 +8,20 @@ class ThemeGenerator {
       colorScheme: ColorScheme(
         brightness: spec.brightness,
         primary: primaryColor,
-        onPrimary: getForegroundColor(primaryColor, spec.backgroundColor, spec.foregroundColor),
+        onPrimary: spec.backgroundOnPrimary ? spec.backgroundColor : spec.foregroundColor,
         primaryContainer: blendColors(primaryColor, spec.backgroundColor, 0.2),
-        onPrimaryContainer: getForegroundColor(primaryColor, spec.backgroundColor, spec.foregroundColor),
+        onPrimaryContainer: spec.backgroundOnPrimary ? spec.backgroundColor : spec.foregroundColor,
         primaryFixed: blendColors(primaryColor, spec.backgroundColor, 0.3),
         primaryFixedDim: blendColors(primaryColor, spec.backgroundColor, 0.1),
-        onPrimaryFixed: getForegroundColor(primaryColor, spec.backgroundColor, spec.foregroundColor),
+        onPrimaryFixed: spec.backgroundOnPrimary ? spec.backgroundColor : spec.foregroundColor,
         onPrimaryFixedVariant: spec.foregroundColor.withAlpha(179),
         secondary: spec.secondaryColor,
-        onSecondary: getForegroundColor(spec.secondaryColor, spec.backgroundColor, spec.foregroundColor),
+        onSecondary: spec.backgroundOnSecondary ? spec.backgroundColor : spec.foregroundColor,
         secondaryContainer: blendColors(spec.secondaryColor, spec.backgroundColor, 0.2),
-        onSecondaryContainer: getForegroundColor(spec.secondaryColor, spec.backgroundColor, spec.foregroundColor),
+        onSecondaryContainer: spec.backgroundOnSecondary ? spec.backgroundColor : spec.foregroundColor,
         secondaryFixed: blendColors(spec.secondaryColor, spec.backgroundColor, 0.3),
         secondaryFixedDim: blendColors(spec.secondaryColor, spec.backgroundColor, 0.1),
-        onSecondaryFixed: getForegroundColor(spec.secondaryColor, spec.backgroundColor, spec.foregroundColor),
+        onSecondaryFixed: spec.backgroundOnSecondary ? spec.backgroundColor : spec.foregroundColor,
         onSecondaryFixedVariant: spec.foregroundColor.withAlpha(179),
         tertiary: spec.foregroundColor.withAlpha(205),
         onTertiary: spec.backgroundColor,
@@ -32,9 +32,9 @@ class ThemeGenerator {
         onTertiaryFixed: spec.backgroundColor,
         onTertiaryFixedVariant: spec.backgroundColor.withAlpha(179),
         error: spec.errorColor,
-        onError: spec.backgroundColor,
+        onError: spec.backgroundOnError ? spec.backgroundColor : spec.foregroundColor,
         errorContainer: blendColors(spec.errorColor, spec.backgroundColor, 0.2),
-        onErrorContainer: spec.backgroundColor,
+        onErrorContainer: spec.backgroundOnError ? spec.backgroundColor : spec.foregroundColor,
         surface: spec.backgroundColor,
         onSurface: spec.foregroundColor,
         surfaceDim: spec.highestSurfaceColor.withAlpha(230),
@@ -98,19 +98,6 @@ class ThemeGenerator {
   static Color blendColors(Color a, Color b, double t) {
     return Color.lerp(a, b, t) ?? a;
   }
-
-  static getForegroundColor(Color backgroundColor, Color c1, Color c2) {
-    final bgLuminance = backgroundColor.computeLuminance();
-    final c1Luminance = c1.computeLuminance();
-    final c2Luminance = c2.computeLuminance();
-
-    if (bgLuminance < 0.5) {
-      return c1Luminance > c2Luminance ? c1 : c2;
-    }
-    else {
-      return c1Luminance < c2Luminance ? c1 : c2;
-    }
-  }
 }
 
 class CustomThemeSpec {
@@ -123,6 +110,10 @@ class CustomThemeSpec {
   final Brightness brightness;
   final bool useMaterial3;
 
+  final bool backgroundOnPrimary;
+  final bool backgroundOnSecondary;
+  final bool backgroundOnError;
+
   CustomThemeSpec({
     required this.backgroundColor,
     required this.foregroundColor,
@@ -132,5 +123,8 @@ class CustomThemeSpec {
     required this.secondaryColor,
     required this.brightness,
     this.useMaterial3 = true,
+    this.backgroundOnPrimary = false,
+    this.backgroundOnSecondary = false,
+    this.backgroundOnError = false,
   });
 }
