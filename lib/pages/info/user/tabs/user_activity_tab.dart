bc7d6deddfd2e5bff49c8239502a5c4b293c275b
Nioduro <mickkcx@proton.me>
2025-11-08 14:49:54 +0100

    feat: add private activity message to user activity tab

--- lib/pages/info/user/tabs/user_activity_tab.dart
+++ lib/pages/info/user/tabs/user_activity_tab.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:vibin_app/sections/section_header.dart';
 import 'package:vibin_app/widgets/entity_card.dart';
+import 'package:vibin_app/widgets/entity_card_row.dart';
 import 'package:vibin_app/widgets/future_content.dart';
 
 import '../../../../api/api_manager.dart';
@@ -21,80 +22,61 @@ class UserActivityTab extends StatelessWidget {
     final lm = AppLocalizations.of(context)!;
 
     return SingleChildScrollView(
-      child: Column(
-        spacing: 16,
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SectionHeader(title: lm.user_activity_last_tracks),
-          FutureContent(
-            future: _activityFuture,
-            hasData: (a) => a.recentTracks.isNotEmpty,
-            height: 210,
-            builder: (context, activity) {
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: activity.recentTracks.length,
-                itemBuilder: (context, index) {
-                  final track = activity.recentTracks[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: EntityCard(
-                      entity: track,
-                      type: EntityCardType.track,
-                    ),
-                  );
-                },
-              );
-            }
-          ),
+      child: FutureContent(
+        future: _activityFuture,
+        hasData: (a) => a.recentTracks.isNotEmpty || a.topTracks.isNotEmpty || a.topArtists.isNotEmpty,
+        errorWidget: Column(
+          spacing: 16,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.visibility_off,
+              size: 64,
+              color: Theme.of(context).colorScheme.onSurfaceVariant,
+            ),
+            Text(
+              lm.user_activity_private,
+              style: TextStyle(color: Theme.of(context).colorScheme.onSurfaceVariant),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+        builder: (context, activity) {
+          return Column(
+            spacing: 16,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SectionHeader(title: lm.user_activity_last_tracks),
+              SizedBox(
+                height: 205,
+                child: EntityCardRow(
+                  entities: activity.recentTracks,
+                  type: EntityCardType.track,
+                ),
+              ),
 
-          SectionHeader(title: lm.user_activity_top_tracks),
-          FutureContent(
-            future: _activityFuture,
-            hasData: (a) => a.topTracks.isNotEmpty,
-            height: 210,
-            builder: (context, activity) {
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: activity.topTracks.length,
-                itemBuilder: (context, index) {
-                  final track = activity.topTracks[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: EntityCard(
-                      entity: track,
-                      type: EntityCardType.track,
-                    ),
-                  );
-                },
-              );
-            }
-          ),
+              SectionHeader(title: lm.user_activity_top_tracks),
+              SizedBox(
+                height: 205,
+                child: EntityCardRow(
+                  entities: activity.topTracks,
+                  type: EntityCardType.track,
+                ),
+              ),
 
-          SectionHeader(title: lm.user_activity_top_artists),
-          FutureContent(
-            future: _activityFuture,
-            hasData: (a) => a.topArtists.isNotEmpty,
-            height: 210,
-            builder: (context, activity) {
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: activity.topArtists.length,
-                itemBuilder: (context, index) {
-                  final artist = activity.topArtists[index];
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: EntityCard(
-                      entity: artist,
-                      type: EntityCardType.artist,
-                    ),
-                  );
-                },
-              );
-            }
-          ),
-        ],
+              SectionHeader(title: lm.user_activity_top_artists),
+              SizedBox(
+                height: 205,
+                child: EntityCardRow(
+                  entities: activity.topArtists,
+                  type: EntityCardType.artist,
+                ),
+              ),
+            ],
+          );
+        }
       ),
     );
   }
